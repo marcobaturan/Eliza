270e03199cdb47ef1d5e3e4c0e088f120234d1b8
Marco Garcia Baturan <marcogb81@hotmail.com>
2017-05-18 17:47:22 +0200

    Add lower case and extend options

--- simphaty.py
+++ simphaty.py
@@ -28,29 +28,29 @@ psychobabble = [
       "¿Realmente te ayudaría a obtener {0}?",
       "¿Está seguro de que necesita {0}?"]],
 
-    [r'¿Por que no puedes ([^\?]*)\??',
+    [r'por que no puedes ([^\?]*)\??' or r'por qué no puedes ([^\?]*)\??',
      ["¿De verdad crees que no lo hago? {0}?",
       "Tal vez eventualmente lo haré. {0}.",
       "¿De verdad quieres que yo {0}?"]],
 
-    [r'¿Por que no puedo ([^\?]*)\??',
+    [r'¿Por que no puedo ([^\?]*)\??' or r'por que no puedo ([^\?]*)\??' or r'¿Por qué no puedo ([^\?]*)\??' or r'Por qué no puedo ([^\?]*)\??',
      ["¿Crees que deberías ser capaz de {0}?",
       "Si pudieras {0}, ¿qué harías?",
       "No sé - ¿por qué no puedes {0}?",
       "¿Lo has intentado realmente?",
       "Si no eres capaz de {0} entonces eso te define cómo un autentico marica!"]],
 
-    [r'No puedo(.*)',
+    [r'no puedo(.*)',
      ["¿Cómo sabes que no puedes{0}?",
       "Tal vez podría {0} si lo intentó.",
       "¿Qué se necesita para que usted {0}?"]],
 
-    [r'Yo soy (.*)',
+    [r'yo soy (.*)',
      ["¿Has venido a mí porque eres  {0}?",
       "¿Cuánto tiempo has estado {0}?",
       "¿Cómo te sientes acerca de ser {0}?"]],
 
-    [r'Estoy (.*)',
+    [r'estoy (.*)',
      ["¿Cómo te hace sentir{0}?",
       "¿Te gusta ser {0}?",
       "¿Por qué me dices que eres{0}?",
@@ -62,42 +62,42 @@ psychobabble = [
       "Tal vez usted piensa que soy.",
       "Puedo ser {0} - ¿qué piensas?"]],
 
-    [r'¿Qué (.*)',
+    [r'¿qué (.*)' or r'qué (.*)' or r'que (.*)',
      ["¿Por qué preguntas?",
       "¿Cómo le ayudaría una respuesta a eso?",
       "¿Qué piensas?"]],
 
-    [r'¿Cómo (.*)',
+    [r'¿cómo (.*)' or r'cómo (.*)' or r'como (.*)',
      ["¿Cómo se supone?",
       "Tal vez puedas responder a tu propia pregunta.",
       "¿Qué estás realmente pidiendo?"]],
 
-    [r'Porque (.*)',
+    [r'porque (.*)',
      ["¿Es esa la verdadera razón?",
       "¿Qué otras razones vienen a la mente?",
       "¿Esa razón se aplica a cualquier otra cosa?",
       "Si {0}, ¿qué otra cosa debe ser verdadera?"]],
 
-    [r'(.*) Lo siento (.*)',
+    [r'(.*) lo siento (.*)',
      ["Hay muchas veces cuando no se necesita disculpa.",
       "¿Qué sentimientos tiene cuando se disculpa?"]],
 
-    [r'Hola(.*)',
+    [r'hola(.*)',
      ["Hola ... me alegro de que puedas pasar por hoy.",
       "¿Hola, cómo estas hoy?",
       "Hola, ¿cómo te sientes hoy?"]],
 
-    [r'Pienso (.*)',
+    [r'pienso (.*)',
      ["¿Dudas {0}?",
       "¿De verdad piensas eso?",
       "Pero no estás seguro {0}?"]],
 
-    [r'(.*) Amigo (.*)',
+    [r'(.*) amigo (.*)',
      ["Cuéntame más sobre tus amigos.",
       "Cuando piensas en un amigo, ¿qué te viene a la mente?",
       "¿Por qué no me hablas de un amigo desde la infancia?"]],
 
-    [r'Si',
+    [r'si',
      ["Pareces muy seguro.",
       "Está bien, pero ¿puedes hacer algo?"]],
 
@@ -107,7 +107,7 @@ psychobabble = [
       "¿Cómo te hacen sentir las computadoras?",
       "¿Se siente amenazado por las computadoras?"]],
 
-    [r'Eso es (.*)',
+    [r'eso es (.*)',
      ["¿Crees que es {0}?",
       "Tal vez sea {0} - ¿qué piensas?",
       "Si fuera {0}, ¿qué harías?",
@@ -117,68 +117,68 @@ psychobabble = [
      ["Pareces muy seguro.",
       "Si te dijera que probablemente no es {0}, ¿qué sentirías?"]],
 
-    [r'¿Puedes ([^\?]*)\??',
+    [r'¿puedes ([^\?]*)\??',
      ["¿Qué te hace pensar que no puedo {0}?",
       "Si pudiera {0}, ¿entonces qué?",
       "¿Por qué me preguntas si puedo (0)?"]],
 
-    [r'Puedo ([^\?]*)\??',
+    [r'puedo ([^\?]*)\??',
      ["No puedes querer {0}.",
       "¿Quieres ser capaz de {0}?",
       "Si pudieras {0}, ¿cierto?"]],
 
-    [r'Tu eres (.*)',
+    [r'tu eres (.*)' or r'¿tu eres (.*)',
      ["¿Por qué crees que soy {0}?",
       "¿Te gusta pensar que soy {0}?",
       "Tal vez usted quiere que sea.",
       "¿Tal vez estás hablando de ti?"]],
 
-    [r'¿Eres ture (.*)',
+    [r'¿eres ture (.*)',
      ["¿Por qué dices que soy {0}?",
       "¿Por qué crees que soy {0}?",
       "¿Estamos hablando de ti o de mí?"]],
 
-    [r'¿No lo hago (.*)',
+    [r'¿no lo hago (.*)',
      ["No realmente {0}?",
       "¿Por qué no {0}?",
       "¿Quieres {0}?"]],
 
-    [r'Me siento (.*)',
+    [r'me siento (.*)',
      ["Bueno, cuéntame más sobre estos sentimientos.",
       "¿Sientes a menudo {0}?",
       "¿Cuándo sientes normalmente {0}?",
       "Cuando sientes {0}, ¿qué haces?"]],
 
-    [r'Yo tengo (.*)',
+    [r'yo tengo (.*)',
      ["¿Por qué me dices que tienes {0}?",
       "¿Realmente tienes {0}?",
       "Ahora que tienes {0}, ¿qué harás después?"]],
 
-    [r'Yo debo (.*)',
+    [r'yo debo (.*)',
      ["¿Podría explicar por qué lo haría?",
       "¿Por que lo harias?",
       "¿Quién más sabe que lo harías?"]],
 
-    [r'Esta ahí (.*)',
+    [r'esta ahí (.*)',
      ["¿Crees que hay {0}?",
       "Es probable que haya {0}.",
       "¿Te gustaría tener {0}?"]],
 
-    [r'Mi (.*)',
+    [r'mi (.*)',
      ["Ya veo, tu {0}.",
       "¿Por qué dices que tu {0}?",
       "Cuando tu {0}, ¿cómo te sientes?"]],
 
-    [r'Tu (.*)',
-     ["Debemos estar discutiendo contigo, no conmigo.",
+    [r'tu (.*)',
+     ["debemos estar discutiendo contigo, no conmigo.",
       "¿Por qué dices eso de mí?",
       "¿Por qué te importa si yo {0}?"]],
 
-    [r'Por qué (.*)',
+    [r'por qué (.*)',
      ["¿Por qué no me dices la razón por la cual {0}?",
       "¿Por qué crees que {0}?"]],
 
-    [r'Quiero que (.*)',
+    [r'quiero que (.*)',
      ["¿Qué significaría para ti si tienes {0}?",
       "¿Por qué quieres {0}?",
       "¿Qué harías si tuvieras {0}?",
@@ -250,7 +250,8 @@ def main():
     print "Hola ¿Cómo te sientes hoy?¿Con resaca?"
 
     while True:
-        statement = raw_input("> ")
+        statement = raw_input("> ").lower()
+
         print analyze(statement)
 
         if statement == "quit":
